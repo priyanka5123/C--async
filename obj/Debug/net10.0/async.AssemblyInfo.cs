c2569b2c9deb2f668cff21c975073a402212f03e
priyanka5123 <priyanka.k8100@gmail.com>
2026-05-20 15:27:59 -0700

    added product

--- obj/Debug/net10.0/async.AssemblyInfo.cs
+++ obj/Debug/net10.0/async.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("async")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c056e0811b1f091db0753c214d5b9e49efa5b2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("async")]
 [assembly: System.Reflection.AssemblyTitleAttribute("async")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
